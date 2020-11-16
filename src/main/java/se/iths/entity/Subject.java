50e2274dd3f7aaac7e7d53e0307f2c4aba34f7cc
toffe <toffe950301@gmail.com>
2020-11-16 10:43:40 +0100

    Added fields + getters and setters in Subject

--- src/main/java/se/iths/entity/Subject.java
+++ src/main/java/se/iths/entity/Subject.java
@@ -1,4 +1,36 @@
 package se.iths.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class Subject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+
+    @NotEmpty
+    @NotNull
+    String subject;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
 }
