4d124e4861cba40bf224ec27a8cf9a8d0d627235
toffe <toffe950301@gmail.com>
2020-11-16 10:54:20 +0100

    Added TeacherService and crud methods

---
+++ src/main/java/se/iths/service/TeacherService.java
@@ -0,0 +1,40 @@
+package se.iths.service;
+
+import se.iths.entity.Teacher;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Transactional
+public class TeacherService {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public Teacher create(Teacher teacher) {
+        entityManager.persist(teacher);
+        return teacher;
+    }
+
+    public Teacher findById(Long id) {
+        return entityManager.find(Teacher.class, id);
+    }
+
+    public List<Teacher> getAll() {
+        return entityManager.createQuery("SELECT t from Teacher t", Teacher.class).getResultList();
+    }
+
+    public void remove(Long id) {
+        Teacher teacher = entityManager.find(Teacher.class, id);
+        entityManager.remove(teacher);
+    }
+
+    public Teacher replaceFields(Teacher teacher) {
+        entityManager.merge(teacher);
+        entityManager.flush();
+        return teacher;
+    }
+
+}
