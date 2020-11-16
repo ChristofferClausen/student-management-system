c62ded5a19f9356efd81da7ce285c2c12e79156a
toffe <toffe950301@gmail.com>
2020-11-16 10:47:56 +0100

    Added fields + getters and setters in Teacher

--- src/main/java/se/iths/entity/Teacher.java
+++ src/main/java/se/iths/entity/Teacher.java
@@ -1,4 +1,31 @@
 package se.iths.entity;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
 public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+
+    @NotEmpty
+    String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
