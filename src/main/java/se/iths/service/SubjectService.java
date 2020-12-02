bffda20fad1a607ba825b2030a7a48d155537bdf
toffe <toffe950301@gmail.com>
2020-12-02 16:05:35 +0100

    Changed addStudent and attTeacher to work the same by taking subjectId and student-/teacherId respectivly

--- src/main/java/se/iths/service/SubjectService.java
+++ src/main/java/se/iths/service/SubjectService.java
@@ -16,19 +16,18 @@ public class SubjectService {
     @PersistenceContext
     EntityManager entityManager;
 
-
-    public Subject addStudent(Long id, Student student) {
+    public Subject addStudent(Long id, Long studentId) {
         var subject = entityManager.find(Subject.class, id);
+        var student = entityManager.find(Student.class, studentId);
         student.addSubject(subject);
         entityManager.merge(subject);
-        entityManager.merge(student);
         return subject;
     }
 
     public Subject addTeacher(Long id, Long teacherId) {
         var subject = entityManager.find(Subject.class, id);
         var teacher = entityManager.find(Teacher.class, teacherId);
-        teacher.addSubject(subject, teacher);
+        teacher.addSubject(subject);
         entityManager.merge(subject);
         return subject;
     }
